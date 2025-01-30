a5967f977bd4e8f1522449add2b0ca6e9cf2e7cc
coji <coji@techtalk.jp>
2025-01-30 22:51:15 +0900

    Refactor API routes to simplify user creation and enhance form handling; update test page for improved submission logic

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,35 +4,46 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useActionState } from 'react'
 import { Form, useForm } from 'react-hook-form'
-import { createPost } from './action'
 import { formSchema, type FormSchema } from './schema'
 
 export default function TestPage() {
   const {
     register,
     control,
-    formState: { errors },
+    reset,
+    formState: { errors, isSubmitting },
   } = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
     progressive: true,
   })
-  const [state, action, pending] = useActionState(createPost, false)
 
   return (
-    <Form control={control} action="/api/post" method="post">
-      <Label htmlFor="title">Title</Label>
-      <Input
-        id="title"
-        {...register('title', { required: 'Title is required' })}
-        type="text"
-        placeholder="title"
-      />
+    <Form
+      control={control}
+      action="/test/api"
+      method="post"
+      onSubmit={(data) => {
+        console.log('onSubmit', data)
+      }}
+      onSuccess={async ({ response }) => {
+        console.log('success!', await response.json())
+        reset()
+      }}
+      className="flex flex-col gap-4"
+    >
+      <fieldset className="flex flex-col gap-1">
+        <Label htmlFor="title">Title</Label>
+        <Input
+          id="title"
+          {...register('title', { required: 'Title is required' })}
+          type="text"
+          placeholder="title"
+        />
+        {errors.title && <p className="text-red-500">{errors.title.message}</p>}
+      </fieldset>
 
-      {errors.title && <p>{errors.title.message}</p>}
-
-      <Button type="submit" disabled={pending}>
+      <Button type="submit" disabled={isSubmitting}>
         Submit
       </Button>
     </Form>
