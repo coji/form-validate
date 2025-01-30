d23d11c3fe0102f9fa154cd71d0cc135aa70ae14
coji <coji@techtalk.jp>
2025-01-30 23:13:12 +0900

    Add Toaster component for notifications and refactor form layout for improved readability

--- src/app/rhf_form/page.tsx
+++ src/app/rhf_form/page.tsx
@@ -1,18 +1,11 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Form, useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { formSchema, type FormSchema } from './schema'
 
 export default function TestPage() {
@@ -31,54 +24,37 @@ export default function TestPage() {
       control={control}
       action="/rhf_form/api"
       method="post"
-      onSubmit={(data) => {
-        console.log('onSubmit', data)
-      }}
       onSuccess={async ({ response }) => {
-        console.log('success!', await response.json())
+        toast.success(`success!: ${JSON.stringify(await response.json())}`)
         reset()
       }}
+      className="grid gap-4"
     >
-      <Card>
-        <CardHeader>
-          <CardTitle>React Hook Form / Form Component</CardTitle>
-          <CardDescription>
-            This is a Form component using React Hook Form
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="grid gap-4">
-          <div className="grid gap-1">
-            <Label htmlFor="name">Name</Label>
-            <Input
-              id="name"
-              {...register('name', { required: 'Name is required' })}
-              type="text"
-              placeholder="title"
-            />
-            {errors.name && (
-              <p className="text-red-500">{errors.name.message}</p>
-            )}
-          </div>
+      <div className="grid gap-1">
+        <Label htmlFor="name">Name</Label>
+        <Input
+          id="name"
+          type="text"
+          placeholder="title"
+          {...register('name', { required: 'Name is required' })}
+        />
+        {errors.name && <p className="text-red-500">{errors.name.message}</p>}
+      </div>
 
-          <div className="grid gap-1">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              {...register('email', { required: 'Email is required' })}
-              type="email"
-              placeholder="email"
-            />
-            {errors.email && (
-              <p className="text-red-500">{errors.email.message}</p>
-            )}
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button type="submit" disabled={isSubmitting}>
-            Submit
-          </Button>
-        </CardFooter>
-      </Card>
+      <div className="grid gap-1">
+        <Label htmlFor="email">Email</Label>
+        <Input
+          id="email"
+          type="email"
+          placeholder="email"
+          {...register('email', { required: 'Email is required' })}
+        />
+        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+      </div>
+
+      <Button type="submit" disabled={isSubmitting}>
+        Submit
+      </Button>
     </Form>
   )
 }
