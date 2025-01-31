dd6de387b1172f1a29b3b01e74dd378f2476299c
coji <coji@techtalk.jp>
2025-01-31 23:50:12 +0900

    Update RouteTitles descriptions for clarity and consistency with new Form component

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -7,11 +7,12 @@ export const RouteTitles = [
   {
     pathname: '/rhf_form',
     title: 'React Hook Form - Form',
-    description: 'Server Side Rendering and Fetch API',
+    description: 'Form component and Fetch API',
   },
   {
     pathname: '/rhf_serveractions',
     title: 'React Hook Form - Server Actions',
-    description: 'React Hook Form with form, useActionState and server actions',
+    description:
+      'Form component / useActionState / Server Actions / useTransition',
   },
 ] as const
