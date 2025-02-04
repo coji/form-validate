9872ac59809c28bd282cf58dad0c91cee930d4ad
coji <coji@techtalk.jp>
2025-02-04 22:48:01 +0900

    Refactor layout in Home component for improved responsiveness and alignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { RouteTitles } from './constants'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+    <div className="grid place-items-center p-8 pb-20 sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
         <Image
           className="dark:invert"
